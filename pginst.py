ba040d1ad81e62b3b0ac5a5113ba018f8f093d2c
Sergey Kovalev <svkoval@mail.ru>
2024-05-10 20:31:02 +0900

    install from package

--- pginst.py
+++ pginst.py
@@ -1,10 +1,10 @@
-# #!/usr/bin/python
+#!/home/sergey/repo/pgsql_install/venv/bin/python3
 
 import sys
 from fabric import Connection
 
 HOST_USER = "admin"
-POSTGRES_VERSON = 15
+POSTGRES_VERSON = 14
 
 commands = {
   "debian": [
@@ -14,7 +14,13 @@ commands = {
     "sudo systemctl restart postgresql",
     "cd ~postgres && sudo -u postgres psql -c 'SELECT 1'"
   ],
-  "centos": []
+  "rhel fedora": [
+    "sudo yum update -y",
+    f"sudo yum install -y postgresql{POSTGRES_VERSON}-server",
+    f"sudo sed -i \"/listen_address/c\listen_addresses = '0.0.0.0'\" /etc/postgresql/{POSTGRES_VERSON}/main/postgresql.conf",
+    "sudo systemctl restart postgresql",
+    "cd ~postgres && sudo -u postgres psql -c 'SELECT 1'"
+  ]
 }
 
 def Usage():
@@ -23,7 +29,10 @@ def Usage():
 def install_pgsql(target):
   conn = Connection(host=target, user=HOST_USER)
   try:
-    os_release_id = "debian"
+    os_release_id = conn.run(". /etc/os-release && echo \"$ID_LIKE\"", hide=True).stdout.strip()
+    if os_release_id == '':
+      os_release_id = conn.run(". /etc/os-release && echo \"$ID\"", hide=True).stdout.strip()
+
     for cmd in commands[os_release_id]:
       conn.run(cmd)
   except Exception as err:
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,8 @@
 # debian 12
-image-id = "fd801rku4j14mv7fs703"
+# image-id = "fd801rku4j14mv7fs703"
 
 # ubuntu 2204
 # image-id = "fd87j6d92jlrbjqbl32q"
+
+# centos-stream-8
+image-id = "fd87jq8nlhdr0uncstqn"
